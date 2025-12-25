4e60cab6cc5a2b8c656375b3391574aa8cac69c3
grypis <grypisnecros@gmail.com>
2025-12-25 12:32:12 +0200

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
-import "./App.css";
+import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
     <div>
       <Profile />
-      <h1>Vite + React</h1>
-      <p>
-        Edit <code>src/App.jsx</code> and save to test HMR
-      </p>
+      <Profile />
+      <Profile />
     </div>
   );
 }
-
-const Profile = () => {
-  return (
-    <>
-      <p>Hi</p>
-      <h2 className="title">Max's Profile</h2>
-    </>
-  );
-};
 
 export default App;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+const Profile = () => {
+  return (
+    <div>
+      <h2>Name: Max</h2>
+      <p>Phone: 123456789</p>
+      <p>ashjcc@example</p>
+      <p>Status: "online"</p>
+    </div>
+  );
+};
+
+export default Profile;
